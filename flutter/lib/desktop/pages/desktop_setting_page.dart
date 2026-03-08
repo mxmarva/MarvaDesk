05112279d1901184892008f2a50cdcb06f90ba5b
Soluciones Tecnologicas de Infraestructura y Sistemas, SAS de CV <soporte@solucionesmarva.com>
2026-03-08 17:18:28 -0600

    Update

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2406,7 +2406,7 @@ class _AboutState extends State<_About> {
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
-                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Purslane Ltd.\n$license',
+                            'Copyright © ${DateTime.now().toString().substring(0, 4)} Soluciones Tecnologicas de Infraestructura y Sistemas, SAS de CV\n$license',
                             style: const TextStyle(color: Colors.white),
                           ),
                           Text(
